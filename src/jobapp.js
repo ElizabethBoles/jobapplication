a1eb2b808717c0bc1e740c1c7a2af7e84ae86aae
elizabeth boles <elizabethboles@elizabeths-MacBook-Air.local>
2017-03-08 16:29:03 -0500

    updated the text value for age range

--- src/jobapp.js
+++ src/jobapp.js
@@ -2,9 +2,9 @@
 
 (function jobApplication() {
     console.log("hello!");
-    document.querySelector('.btn-warning').addEventListener('click', function printHello(eventObj) {
+    document.querySelector('.btn-warning').addEventListener('click', function printHello(evt) {
     console.log("i love to code");
-    console.log(eventObj);
+    console.log(evt);
     });
     document.querySelector("#full-name").addEventListener('blur', function fullName(evt) {
       console.log(evt.target.value.length);
@@ -14,5 +14,19 @@
       } else {
         evt.target.parentNode.parentNode.classList.add("has-error");
       }
-    });
+
+
+     });
+      //When the range input ("experience") is changed, update the text in the figure
+       //with the correct value from the input.
+
+        document.querySelector("#exp").addEventListener('change', function experience(evt) {
+           evt.target.parentNode.childNodes[3].innerText = evt.target.value + " years";
+
+
+
+
+       //function needs to be above
+       //So if I slide the dot to the right 1 step the figure should read "6 years".
+      });
 })();
